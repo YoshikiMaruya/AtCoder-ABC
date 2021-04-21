19b0edf7521b3b420699248cee1c849b6e7bd593
YoshikiMaruya <yoshimaru0514@gmail.com>
2021-04-21 23:44:52 +0900

    coding test

---
+++ freee/main.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define reps(i, s, n) for (int i = (s); i < (n); i++)
+using namespace std;
+using ll = long long;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+#define MOD 998244353
+
+int main(){
+  string r,s,dir,hh;
+
+  cin>>r>>s>>dir>>hh;
+
+  int a_weight[12]={3,5,2,3,4,3,4,2,2,3,6,2};
+  int b_weight[5]={4,3,3,2,3};
+
+  if(s=="A2"){
+    if(dir=="U"){
+      for(int i=55;i<=60;i+=5){
+        cout<<i<<" ";
+      }
+    }
+  }
+
+  cout<<endl;
+  return 0;
+}
