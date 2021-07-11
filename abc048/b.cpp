b650a972706b1e2b92e49c871d61b38d22944253
YoshikiMaruya <yoshimaru0514@gmail.com>
2021-07-11 23:52:00 +0900

    solved the abc048

---
+++ abc048/b.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define reps(i, s, n) for (int i = (s); i < (n); i++)
+using namespace std;
+using ll = long long;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+#define MOD 998244353
+
+int main(){
+  ll a,b,x;
+  cin>>a>>b>>x;
+
+  ll solve=b/x-a/x;
+  if(a%x==0) solve++;
+  cout<<solve<<endl;
+  return 0;
+}
